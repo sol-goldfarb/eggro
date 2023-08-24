b604eb3d6f7ceba8a2d686ba90b0b339482b53c3
Solomon Goldfarb <sol.goldfarb@gmail.com>
2023-08-24 15:06:02 -0700

    Implement code that reads year picker and base year from text entry field, then calculates when button pressed

--- eggro/Controllers/ViewController.swift
+++ eggro/Controllers/ViewController.swift
@@ -15,21 +15,56 @@ class ViewController: UIViewController, UIPickerViewDataSource {
     var selectedYear: Int?
     var baseSpend: Int?
     var yearArray: [Int] = []
+    var cpiDataArray: [[Any]] = [[]]
 
     @IBOutlet weak var yearPicker: UIPickerView!
     
     @IBOutlet weak var baseSpendTextField: UITextField!
     
-    @IBOutlet weak var infAdjSpendLabel: UILabel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         yearPicker.dataSource = self
         yearPicker.delegate = self
         yearPicker.isHidden = true
+        
+        baseSpendTextField.delegate = self
+        baseSpendTextField.placeholder = "Pre-tax spending in base year"
+        
         retrieveParseData.delegate = self
         retrieveParseData.fetchData()
+        
     }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        self.baseSpend = Int(baseSpendTextField.text ?? "Error")
+        if let baseYear = self.selectedYear, let baseSpend = self.baseSpend {
+            print("The base year is \(baseYear).")
+            let yearsSinceBase = self.cpiDataArray[0][0] as! Int - baseYear
+            let eggroCalcs = self.eggroCalcBrain.performCalcs(cpiDataArray: cpiDataArray, baseYear: baseYear, baseSpend: baseSpend)
+            
+            print("The most recently available data is from \(cpiDataArray[0][2]) \(cpiDataArray[0][0]).")
+            
+            print("Pre-tax spending in \(baseYear) was $\(baseSpend.withCommas()).")
+            
+            print("Inflation since \(cpiDataArray[12 * yearsSinceBase][2]) \(cpiDataArray[12 * yearsSinceBase][0]) is \(String(format: "%.2f", (eggroCalcs.infAdj)))%.")
+            
+            print("The inflation adjusted spend is $\(eggroCalcs.infAdjSpend.withCommas()).")
+            
+            print("The inflation adjusted spend is $\(eggroCalcs.infAdjSpend.withCommas()).")
+            
+            print("Base Year Target Investments (4% Withdrawal) = $\(eggroCalcs.baseTargetInvLow.withCommas()).")
+            
+            print("Base Year Target Investments (3% Withdrawal) = $\(eggroCalcs.baseTargetInvHigh.withCommas()).")
+            
+            print("Inflation Adjusted Target Investments (4% Withdrawal) = $\(eggroCalcs.infAdjTargetInvLow.withCommas()).")
+            
+            print("Inflation Adjusted Target Investments (3% Withdrawal) = $\(eggroCalcs.infAdjTargetInvHigh.withCommas()).")
+            
+        }
+        
+    }
+    
 }
 
 
@@ -48,8 +83,7 @@ extension ViewController: RetrieveParseDataDelegate, UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        var selectedYear = self.yearArray[row]
-//        coinManager.getCoinPrice(cryptoType: "BTC", currency: selectedCurrency)
+        return selectedYear = self.yearArray[row]
     }
 
     func didFailWithError(error: Error) {
@@ -59,92 +93,39 @@ extension ViewController: RetrieveParseDataDelegate, UIPickerViewDelegate {
     
     func didRetrieveParseData(dataArray: [[Any]]) {
         
+        self.cpiDataArray = dataArray
         self.yearArray = ((dataArray[0][0] as! Int - 10)...(dataArray[0][0] as! Int)).map { $0 }
         
-        baseYear = 2017
-        baseSpend = 70000
-        
         DispatchQueue.main.async {
-
+            
             self.yearPicker.isHidden = false
             self.yearPicker.reloadAllComponents()
-
-            if let baseYear = self.baseYear, let baseSpend = self.baseSpend {
-                
-                let yearsSinceBase = dataArray[0][0] as! Int - baseYear
-                let eggroCalcs = self.eggroCalcBrain.performCalcs(cpiDataArray: dataArray, baseYear: baseYear, baseSpend: baseSpend)
-                
-                print("The most recently available data is from \(dataArray[0][2]) \(dataArray[0][0]).")
-                
-                print("Pre-tax spending in \(baseYear) was $\(baseSpend.withCommas()).")
-                
-                print("Inflation since \(dataArray[12 * yearsSinceBase][2]) \(dataArray[12 * yearsSinceBase][0]) is \(String(format: "%.2f", (eggroCalcs.infAdj)))%.")
-                
-                print("The inflation adjusted spend is $\(eggroCalcs.infAdjSpend.withCommas()).")
-                
-                self.infAdjSpendLabel.text = String("The inflation adjusted spend is $\(eggroCalcs.infAdjSpend.withCommas()).")
-                
-                print("Base Year Target Investments (4% Withdrawal) = $\(eggroCalcs.baseTargetInvLow.withCommas()).")
-                
-                print("Base Year Target Investments (3% Withdrawal) = $\(eggroCalcs.baseTargetInvHigh.withCommas()).")
-                
-                print("Inflation Adjusted Target Investments (4% Withdrawal) = $\(eggroCalcs.infAdjTargetInvLow.withCommas()).")
-                
-                print("Inflation Adjusted Target Investments (3% Withdrawal) = $\(eggroCalcs.infAdjTargetInvHigh.withCommas()).")
-                
-            }
+            
         }
     }
 }
 
 extension ViewController: UITextFieldDelegate {
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        baseSpendTextField.endEditing(true)
-        return true
-    }
-   
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let baseSpend = baseSpendTextField.text {
-/*            let cityWithNoSpaces: String = city.replacingOccurrences(of: " ", with: "%20")
-            weatherManager.fetchWeather(cityName: cityWithNoSpaces)
- */
+        if baseSpendTextField.text != "" {
+            self.view.endEditing(true)
+            return true
+        } else {
+            return false
         }
-        baseSpendTextField.text = "Pre-tax spending in base year"
     }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if baseSpendTextField.text != "" {
+            self.view.endEditing(true)
+            return true
+        } else {
+            return false
+        }
+    }
 }
-
-/*
- extension ViewController: UITextFieldDelegate {
-     
-     @IBAction func searchPressed(_ sender: UIButton) {
-         searchTextField.endEditing(true)
-     }
-     
-     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-         searchTextField.endEditing(true)
-         return true
-     }
-     
-     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-         if textField.text != "" {
-             textField.placeholder = "Search for a City"
-             return true
-         } else {
-             textField.placeholder = "Enter a City Name"
-             return false
-         }
-     }
-     
-     func textFieldDidEndEditing(_ textField: UITextField) {
-         if let city = searchTextField.text {
-             let cityWithNoSpaces: String = city.replacingOccurrences(of: " ", with: "%20")
-             weatherManager.fetchWeather(cityName: cityWithNoSpaces)
-         }
-         searchTextField.text = ""
-     }
- }
- */
+    
 
 extension Int {
     func withCommas() -> String {
@@ -153,3 +134,5 @@ extension Int {
         return numberFormatter.string(from: NSNumber(value:self))!
     }
 }
+
+
